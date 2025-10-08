06ca07eda64a26911566be11f344d74e3f92d9e1
Ivann <ivann.machado@laplateforme.io>
2025-10-08 16:05:40 +0200

    job12 init

---
+++ job-12/Electronic.php
@@ -0,0 +1,30 @@
+<?php
+class Electronic extends Product {
+	private string $brand;
+	private int $warranty_fee;
+
+	public function __construct(string $brand = null, int $warranty_fee = null, int $id = null, int $category_id = null, string $name = null, array $photo = null, int $price = null, string $description = null, int $quantity = null, DateTime $createdAt = null, DateTime $updatedAt = null) {
+		parent::__construct($id, $category_id, $name, $photo, $price, $description, $quantity, $createdAt, $updatedAt);
+		$this->hydrate([
+			'brand' => $brand,
+			'warranty_fee' => $warranty_fee
+		]);
+	}
+
+	public function getBrand(): string {
+		return $this->brand;
+	}
+
+	public function setBrand(string $brand): void {
+		$this->brand = $brand;
+	}
+
+	public function getWarranty_fee(): int {
+		return $this->warranty_fee;
+	}
+
+	public function setWarranty_fee(int $warranty_fee): void {
+		$this->warranty_fee = $warranty_fee;
+	}
+}
+?>
