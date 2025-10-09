00e394dfad421126c23c778cee2e4e928f6ac791
Ivann <ivann.machado@laplateforme.io>
2025-10-09 13:50:03 +0200

    pdo fix

--- job-13/Database.php
+++ job-13/Database.php
@@ -11,7 +11,7 @@ class Database {
 			return self::$pdo;
 		}
 		try {
-			self::$pdo = new PDO('mysql:host='.self::$dbHost.';dbname='.self::$dbName.', '.self::$dbUser.', '.self::$dbPass);
+			self::$pdo = new PDO('mysql:host='.self::$dbHost.';dbname='.self::$dbName, self::$dbUser, self::$dbPass);
 			self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 			self::$pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
 			self::$pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
